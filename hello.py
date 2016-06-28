5689f86f5f8e3f672e110c5075dafed9e5d80fe4
unknown <kylefl@SBALKYLEFL01.citrite.net>
2016-06-27 22:07:26 -0700

    Add templates.

--- hello.py
+++ hello.py
@@ -1,14 +1,31 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "super secret"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("You changed your name!")
+        session['name'] = form.name.data
+        form.name.data = ""
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get('name'))
 
 @app.route("/user/<name>")
 def user(name):
